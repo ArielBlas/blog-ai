cb4685361e5c88ecfbe40a7668bd46068d47109b
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-06-14 23:40:33 -0300

    feat: loader component

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -4,6 +4,7 @@ import { assets, blog_data, comments_data } from "../assets/assets";
 import Navbar from "../components/Navbar";
 import Moment from "moment";
 import Footer from "../components/Footer";
+import Loader from "../components/Loader";
 
 interface BlogData {
   _id: string;
@@ -167,7 +168,7 @@ const Blog = () => {
       <Footer />
     </div>
   ) : (
-    <div>Loading...</div>
+    <Loader />
   );
 };
 
