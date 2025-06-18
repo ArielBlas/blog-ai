2582f6f5936de31518697c95b4ef5fcc4d6a533d
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-06-17 23:56:30 -0300

    feat: admin sidebar component

--- src/pages/admin/Layout.tsx
+++ src/pages/admin/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { assets } from "../../assets/assets";
-import { useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import Sidebar from "../../components/admin/Sidebar";
 
 const Layout = () => {
   const navigate = useNavigate();
@@ -28,6 +29,10 @@ const Layout = () => {
         >
           Logout
         </button>
+      </div>
+      <div className="flex h-[calc(100vh-70px)]">
+        <Sidebar />
+        <Outlet />
       </div>
     </>
   );
