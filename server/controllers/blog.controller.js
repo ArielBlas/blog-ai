241bd85a52880d7d8baedb1ee3aee7c048b3627c
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-07-01 23:57:23 -0300

    feat: imaagekit config

--- server/controllers/blog.controller.js
+++ server/controllers/blog.controller.js
@@ -1,9 +1,12 @@
 export const addBlog = async (req, res) => {
   try {
-    const { title, subTitle, description, category, image, isPublished } =
-      req.body;
+    const { title, subTitle, description, category, isPublished } = JSON.parse(
+      req.body.body
+    );
 
-    if (!title || !description || !category || !image) {
+    const imageFile = req.file;
+
+    if (!title || !description || !category || !imageFile) {
       return res.json({ success: false, message: "Missing required fields" });
     }
   } catch (error) {}
