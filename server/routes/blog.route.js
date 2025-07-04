6ec8d83e9c9c7f0805f1aa08e19f8e0f77482bf8
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-07-03 23:52:53 -0300

    feat: multer middleware

--- server/routes/blog.route.js
+++ server/routes/blog.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { addBlog } from "../controllers/blog.controller";
+import upload from "../middleware/multer";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add", addBlog);
+blogRouter.post("/add", upload.single("image"), addBlog);
 
 export default blogRouter;
