ae0481d6b2738b28b39d626e66021f819603b12b
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-07-04 23:52:53 -0300

    feat: auth middleware

--- server/routes/blog.route.js
+++ server/routes/blog.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { addBlog } from "../controllers/blog.controller";
 import upload from "../middleware/multer";
+import auth from "../middleware/auth";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add", upload.single("image"), addBlog);
+blogRouter.post("/add", upload.single("image"), auth, addBlog);
 
 export default blogRouter;
