32b4f75b43e650e75c237f5561b73e1e6483b98e
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-07-05 23:23:58 -0300

    feat: blog crud

--- server/routes/blog.route.js
+++ server/routes/blog.route.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { addBlog } from "../controllers/blog.controller";
+import {
+  addBlog,
+  deleteBlogById,
+  getAllBlogs,
+  getBlogById,
+  togglePublish,
+} from "../controllers/blog.controller";
 import upload from "../middleware/multer";
 import auth from "../middleware/auth";
 
 const blogRouter = express.Router();
 
 blogRouter.post("/add", upload.single("image"), auth, addBlog);
+blogRouter.get("/all", getAllBlogs);
+blogRouter.get("/:blogId", getBlogById);
+blogRouter.post("/:blogId", auth, deleteBlogById);
+blogRouter.post("/toggle-publish", auth, togglePublish);
 
 export default blogRouter;
