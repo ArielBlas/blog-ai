f5a4da1b5a410e7c10f7b00d4f9f16bcbd86d28a
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-07-07 23:18:48 -0300

    feat: blog comments

--- server/routes/blog.route.js
+++ server/routes/blog.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {
   addBlog,
+  addComment,
   deleteBlogById,
   getAllBlogs,
   getBlogById,
+  getBlogComments,
   togglePublish,
 } from "../controllers/blog.controller";
 import upload from "../middleware/multer";
@@ -16,5 +18,7 @@ blogRouter.get("/all", getAllBlogs);
 blogRouter.get("/:blogId", getBlogById);
 blogRouter.post("/:blogId", auth, deleteBlogById);
 blogRouter.post("/toggle-publish", auth, togglePublish);
+blogRouter.post("/add-comment", addComment);
+blogRouter.post("/comments", getBlogComments);
 
 export default blogRouter;
