a7e091cb25ee2d8639c9fc75c3341607090dda2b
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-07-17 23:58:36 -0300

    feat: add blog state

--- client/src/pages/admin/AddBlog.tsx
+++ client/src/pages/admin/AddBlog.tsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState, useEffect, useRef, use } from "react";
+import { assets, blogCategories } from "../../assets/assets";
+import Quill from "quill";
 
 const AddBlog = () => {
+  const editorRef = useRef<HTMLDivElement>(null);
+  const quillRef = useRef<Quill | null>(null);
+
+  const [image, setImage] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>("");
+  const [subTitle, setSubTitle] = useState<string>("");
+  const [category, setCategory] = useState<string>("Startup");
+  const [isPublished, setIsPublished] = useState<boolean>(false);
+
+  const generateContent = () => {};
+
+  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, {
+        theme: "snow",
+      });
+    }
+  }, []);
+
   return <div>AddBlog</div>;
 };
 
